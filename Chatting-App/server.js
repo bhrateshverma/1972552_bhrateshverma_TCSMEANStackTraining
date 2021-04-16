cba5799057eda7e926a5ce6c4a94295a49f2842a
bhrateshverma <bru.verma77@gmail.com>
2021-04-15 19:19:02 -0500

    Chatting app completed

---
+++ Chatting-App/server.js
@@ -0,0 +1,21 @@
+let app = require("express")();
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+app.get("/",(req,res)=> {
+    res.sendFile(__dirname+"/index.html");
+})
+
+io.on("connection",(socket)=> {
+    socket.on("send message",(msg)=>{
+        console.log(msg);
+    })
+})
+
+/*io.on("connection",(socket1)=> {
+    socket1.on("message",(msg)=>{
+        console.log(msg);
+    })
+}) */
+
+http.listen(9090,()=>console.log('server running on port number 9090'));
